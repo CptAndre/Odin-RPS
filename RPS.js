b4085a899de6446dc8aef337ba87a046373b704e
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-06 17:14:18 -0300

    Created Human choice function

--- RPS.js
+++ RPS.js
@@ -1,10 +1,26 @@
-for(let round=0; round<5; round++){
+for (let round=0; round<5; round++) {
     
-    function computerChoice(){
+    let choice = prompt("Chose Rock Paper or Scissors, relember is case sensitive");
+
+    function computerChoice() {
         let Comp = Math.round(Math.random() * 2);
         return Comp;
     }
 
     console.log(computerChoice());
+
+    function humanChoice(){
+        if(choice === "Rock"){
+            return "Rock";
+        }
+        else if (choice === "Paper"){
+            return "Paper";
+        }
+        else{
+            return "Scissors";
+        }
+    }
+
+    console.log(humanChoice());
 
 }
