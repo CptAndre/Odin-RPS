0f3a811715a717b3ca5ce43dfdb316b22574f3d8
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-07 08:51:26 -0300

    Reworked the code to have round to be in a function

--- RPS.js
+++ RPS.js
@@ -1,45 +1,52 @@
 let compscore=0 , humscore=0;
 
-for (let round=0; round<5; round++) {
-    
+function computerChoice() {
+    let Comp = Math.floor(Math.random() * 3)
+    return Comp;
+    }
+
+function humanChoice(choice){
+    if(choice === "Rock"){
+        return 0;
+    }
+    else if (choice === "Paper"){
+        return 1;
+    }
+    else{
+        return 2;
+    }
+}
+
+function playRound(){
+    for (let round=1; round<=5; round++) {
+        
     let choice = prompt("Chose Rock Paper or Scissors, relember is case sensitive");
+    console.log(`Current round: ${round}`);
 
-    function computerChoice() {
-        let Comp = Math.round(Math.random() * 2);
-        return Comp;
-    }
-
-    function humanChoice(){
-        if(choice === "Rock"){
-            return 0;
-        }
-        else if (choice === "Paper"){
-            return 1;
-        }
-        else{
-            return 2;
-        }
-    }
-
-    let human = humanChoice();
+    let human = humanChoice(choice);
     let computer  = computerChoice();
 
-        if (human === computer ){
-            console.log("Tie, no points for anybody");
-        }
+    if (human === computer){
+        console.log("Tie, no points for anybody");
+    }
 
-        else if((human === 0 && computer === 2)||
-                 (human === 1 && computer === 0) || 
-                 (human === 2 && computer === 1)){
+    else if(
+            (human === 0 && computer === 2) ||
+            (human === 1 && computer === 0) || 
+            (human === 2 && computer === 1)
+            ){
             console.log("Victory! Point for you");
             humscore++;
+    }
 
-        }
-
-        else{
-            console.log("Defeat! Point for computer");
-            compscore++;
-        }
+    else{
+        console.log("Defeat! Point for computer");
+        compscore++;
+    }
         
-        console.log(`Current Score: ${humscore} vs ${compscore}`);
+    console.log(`Current Score: ${humscore} vs ${compscore}`);
+
+    }
 }
+
+playRound();
