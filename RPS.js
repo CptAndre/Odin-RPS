baffc246c9fff133c5e8a5a4e73111e68c1ade88
CptAndre <138729600+CptAndre@users.noreply.github.com>
2026-04-07 07:11:08 -0300

    Changed the human choice to have better logic with the overall scoring

--- RPS.js
+++ RPS.js
@@ -1,3 +1,5 @@
+let compscore=0 , humscore=0;
+
 for (let round=0; round<5; round++) {
     
     let choice = prompt("Chose Rock Paper or Scissors, relember is case sensitive");
@@ -7,20 +9,37 @@ for (let round=0; round<5; round++) {
         return Comp;
     }
 
-    console.log(computerChoice());
-
     function humanChoice(){
         if(choice === "Rock"){
-            return "Rock";
+            return 0;
         }
         else if (choice === "Paper"){
-            return "Paper";
+            return 1;
         }
         else{
-            return "Scissors";
+            return 2;
         }
     }
 
-    console.log(humanChoice());
+    let human = humanChoice();
+    let computer  = computerChoice();
 
+        if (human === computer ){
+            console.log("Tie, no points for anybody");
+        }
+
+        else if((human === 0 && computer === 2)||
+                 (human === 1 && computer === 0) || 
+                 (human === 2 && computer === 1)){
+            console.log("Victory! Point for you");
+            humscore++;
+
+        }
+
+        else{
+            console.log("Defeat! Point for computer");
+            compscore++;
+        }
+        
+        console.log(`Current Score: ${humscore} vs ${compscore}`);
 }
